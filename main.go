fe9023646295639a18d516b6d8528098dfe6e1bd
Praveen raj Mani <hackerpraveen@gmail.com>
2017-12-05 10:04:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -49,8 +49,8 @@ func NewBlockchain() *Blockchain {
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	bc.AddBlock("Send 1 BTC to A")
+	bc.AddBlock("Send 2 more BTC to A")
 
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
